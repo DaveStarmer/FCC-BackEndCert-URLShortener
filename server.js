9fd9261b8f8fbd342a12feae55ed83d288f49b38
Dave Starmer <dave@starmer.org.uk>
2022-03-24 00:18:07 +0000

    Code

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+
+// array of shortened URLs
+const urlList = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,6 +14,9 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+// add body parsing
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -17,6 +24,39 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl',(req,res)=>{
+  const num = urlList.length;
+  const urlTxt = req.body.url;
+  try {
+    if (!(urlTxt.match(/^([A-Za-z]+\:\/\/)/))){
+      res.json({ error: 'invalid url' });
+    }
+    else {
+      const longUrl = new URL(urlTxt);
+      urlList.push(longUrl);
+      res.json({ "original_url": longUrl, "short_url": num });
+    }
+  }
+  catch (err) {
+    if (err.code == "ERR_INVALID_URL"){
+      res.json({ error: 'invalid url' });
+    }
+    else {
+      res.json({ error: err.code });
+    }
+  }
+});
+
+app.get('/api/shorturl/:short',(req,res)=>{
+  const idx = parseInt(req.params.short);
+  if (idx >=0 && idx < urlList.length) {
+    res.redirect(urlList[idx]);
+  }
+  else {
+    res.status(404).send("Not Found");
+  }
 });
 
 app.listen(port, function() {
